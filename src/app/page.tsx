47f9d47deee753d1ade07bf758a87206fc94b5b3
Matheus297 <matheusabreu297@gmail.com>
2024-10-07 12:15:19 -0300

    ajustando pklayerref

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -112,16 +112,17 @@ export default function Home() {
 
 
   const onPlayerReadyYoutube: any = () => {
-    const player = playerRef.current;
-    console.log('Opa', player.getPlayerState())
-
-    // Verifica se o vídeo terminou (estado 0 - video ended)
-    if (player.getPlayerState() === 1) {
-      player.pauseVideo();
+    const player: any = playerRef?.current;
+  
+    if (player && typeof player.getPlayerState === 'function') {
+      // Verifica se o vídeo está em reprodução (estado 1 - playing)
+      if (player.getPlayerState() === 1) {
+        player.pauseVideo();  // Pausa o vídeo se estiver em reprodução
+      } else {
+        player.playVideo();  // Inicia a reprodução se não estiver tocando
+      }
     } else {
-      // Adicione aqui o que você deseja fazer se o vídeo não estiver no estado "encerrado"
-      // Por exemplo, começar a tocar o vídeo
-      player.playVideo();  // Inicia a reprodução se o vídeo não estiver encerrado
+      console.error("Player is not ready or getPlayerState is not a function");
     }
   };
 
